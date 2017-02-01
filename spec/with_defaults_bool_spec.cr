177e531b5527a31e01ef9f999618809c973e7ebf
Konstantin Makarchev <kostya27@gmail.com>
2017-02-02 01:08:07 +0300

    more specs

--- spec/with_defaults_bool_spec.cr
+++ spec/with_defaults_bool_spec.cr
@@ -10,40 +10,40 @@ struct WithDefaultsBoolFalse
   field :x, Bool, default: false
 end
 
-# describe "AutoConstructor" do
-#   context "WithDefaultsBoolTrue" do
-#     it { WithDefaultsBoolTrue.new(true).x.should eq true }
-#     it { WithDefaultsBoolTrue.new.x.should eq true }
-#     it { WithDefaultsBoolTrue.new(false).x.should eq false }
+describe "AutoConstructor" do
+  context "WithDefaultsBoolTrue" do
+    it { WithDefaultsBoolTrue.new(true).x.should eq true }
+    it { WithDefaultsBoolTrue.new.x.should eq true }
+    it { WithDefaultsBoolTrue.new(false).x.should eq false }
 
-#     it { WithDefaultsBoolTrue.new(x: true).x.should eq true }
-#     it { WithDefaultsBoolTrue.new(x: false).x.should eq false }
-#     it { WithDefaultsBoolTrue.new(y: false).x.should eq true }
+    # it { WithDefaultsBoolTrue.new(x: true).x.should eq true }
+    # it { WithDefaultsBoolTrue.new(x: false).x.should eq false }
+    # it { WithDefaultsBoolTrue.new(y: false).x.should eq true }
 
-#     it { WithDefaultsBoolTrue.new({:x => true}).x.should eq true }
-#     it { WithDefaultsBoolTrue.new({:x => false}).x.should eq false }
-#     it { WithDefaultsBoolTrue.new({:y => nil}).x.should eq true }
+    it { WithDefaultsBoolTrue.new({:x => true}).x.should eq true }
+    it { WithDefaultsBoolTrue.new({:x => false}).x.should eq false }
+    it { WithDefaultsBoolTrue.new({:y => nil}).x.should eq true }
 
-#     it { WithDefaultsBoolTrue.new({"x" => true}).x.should eq true }
-#     it { WithDefaultsBoolTrue.new({"x" => false}).x.should eq false }
-#     it { WithDefaultsBoolTrue.new({"y" => nil}).x.should eq true }
-#   end
+    it { WithDefaultsBoolTrue.new({"x" => true}).x.should eq true }
+    it { WithDefaultsBoolTrue.new({"x" => false}).x.should eq false }
+    it { WithDefaultsBoolTrue.new({"y" => nil}).x.should eq true }
+  end
 
-#   context "WithDefaultsBoolFalse" do
-#     it { WithDefaultsBoolFalse.new(true).x.should eq true }
-#     it { WithDefaultsBoolFalse.new.x.should eq false }
-#     it { WithDefaultsBoolFalse.new(false).x.should eq false }
+  context "WithDefaultsBoolFalse" do
+    # it { WithDefaultsBoolFalse.new(true).x.should eq true }
+    # it { WithDefaultsBoolFalse.new.x.should eq false }
+    # it { WithDefaultsBoolFalse.new(false).x.should eq false }
 
-#     it { WithDefaultsBoolFalse.new(x: true).x.should eq true }
-#     it { WithDefaultsBoolFalse.new(x: false).x.should eq false }
-#     it { WithDefaultsBoolFalse.new(y: false).x.should eq fals }
+    # it { WithDefaultsBoolFalse.new(x: true).x.should eq true }
+    # it { WithDefaultsBoolFalse.new(x: false).x.should eq false }
+    # it { WithDefaultsBoolFalse.new(y: false).x.should eq fals }
 
-#     it { WithDefaultsBoolFalse.new({:x => true}).x.should eq true }
-#     it { WithDefaultsBoolFalse.new({:x => false}).x.should eq false }
-#     it { WithDefaultsBoolFalse.new({:y => nil}).x.should eq false }
+    # it { WithDefaultsBoolFalse.new({:x => true}).x.should eq true }
+    # it { WithDefaultsBoolFalse.new({:x => false}).x.should eq false }
+    # it { WithDefaultsBoolFalse.new({:y => nil}).x.should eq false }
 
-#     it { WithDefaultsBoolFalse.new({"x" => true}).x.should eq true }
-#     it { WithDefaultsBoolFalse.new({"x" => false}).x.should eq false }
-#     it { WithDefaultsBoolFalse.new({"y" => nil}).x.should eq false }
-#   end
-# end
+    # it { WithDefaultsBoolFalse.new({"x" => true}).x.should eq true }
+    # it { WithDefaultsBoolFalse.new({"x" => false}).x.should eq false }
+    # it { WithDefaultsBoolFalse.new({"y" => nil}).x.should eq false }
+  end
+end
