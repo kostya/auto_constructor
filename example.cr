38d3d405b982f8933a82a00dcf4703f8f0228b27
Konstantin Makarchev <kostya27@gmail.com>
2017-02-01 21:14:22 +0400

    add example

--- example.cr
+++ example.cr
@@ -1,21 +1,4 @@
-# auto_constructor
-
-Auto construct initialize methods for classes and structs
-
-## Installation
-
-Add this to your application's `shard.yml`:
-
-```yaml
-dependencies:
-  auto_constructor:
-    github: kostya/auto_constructor
-```
-
-## Usage
-
-```crystal
-require "auto_constructor"
+require "./src/auto_constructor"
 
 struct A
   include AutoConstructor
@@ -32,4 +15,3 @@ p A.new(a: 1, b: "what", c: 3, d: "bla", e: 1.0) # => A(@a=1, @b="what", @c=3, @
 p A.new(a: 1, c: 3, e: 1.0)                      # => A(@a=1, @b="def", @c=3, @d=nil, @e=1.0)
 p A.new({:a => 1, :c => 3, :e => 1.0})           # => A(@a=1, @b="def", @c=3, @d=nil, @e=1.0)
 p A.new({"a" => 1, "c" => 3, "e" => 1.0})        # => A(@a=1, @b="def", @c=3, @d=nil, @e=1.0)
-```
