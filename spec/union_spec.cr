f4a32db84a40eb3a3f684e2cd89177ee83ec8bad
Konstantin Makarchev <kostya27@gmail.com>
2017-02-02 06:30:39 +0300

    one more spec

--- spec/union_spec.cr
+++ spec/union_spec.cr
@@ -6,9 +6,19 @@ struct UnionTest
   field :a, Int32 | String
 end
 
+struct UnionTestNil
+  include AutoConstructor
+
+  field :a, Int32 | String | Nil, default: nil
+end
+
 describe AutoConstructor do
   it { UnionTest.new(1).a.should eq 1 }
   it { UnionTest.new("1").a.should eq "1" }
   it { UnionTest.new(a: 1).a.should eq 1 }
   it { UnionTest.new(a: "1").a.should eq "1" }
+
+  it { UnionTestNil.new(1).a.should eq 1 }
+  it { UnionTestNil.new("1").a.should eq "1" }
+  it { UnionTestNil.new.a.should eq nil }
 end
