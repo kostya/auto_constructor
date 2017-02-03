be47d1ac236bb1faec78ae6dea13f4faa13739f0
Konstantin Makarchev <kostya27@gmail.com>
2017-02-03 19:57:14 +0400

    fix

--- spec/double_include_spec.cr
+++ spec/double_include_spec.cr
@@ -24,6 +24,16 @@ end
 module Bla3
   include Bla2
   field :x, Int32
+
+  macro finished
+    def bla
+      s = ""
+      \{% for field in AUTO_CONSTRUCTOR_FIELDS %}
+        s += \{{ field[:name].id.stringify }}
+      \{% end %}
+      s
+    end
+  end
 end
 
 class DoubleInclude3
@@ -60,9 +70,12 @@ describe AutoConstructor do
 
   it { DoubleInclude3.new(1, "2").xy.should eq({1, "2"}) }
   it { DoubleInclude3.new(y: "2", x: 1).xy.should eq({1, "2"}) }
+  it { DoubleInclude3.new(y: "2", x: 1).bla.should eq "xy" }
 
   it { DoubleInclude4.new(1, "2", "z").xyz.should eq({1, "2", "z"}) }
   it { DoubleInclude4.new(y: "2", x: 1, z: "z").xyz.should eq({1, "2", "z"}) }
+  it { DoubleInclude4.new(y: "2", x: 1, z: "z").bla.should eq "xyz" }
 
   it { DoubleInclude5.new(1, "2", "z", 2).xyzd.should eq({1, "2", "z", 2}) }
+  it { DoubleInclude5.new(1, "2", "z", 2).bla.should eq "xyzd" }
 end
